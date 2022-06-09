d24b6e6029c89a5d843483490e435cec6b1ed7c4
Luis Fernando Benatto <luisbenatto5@gmail.com>
2022-06-09 00:16:53 -0300

    First Steps in Image Processing

--- src/imageProcessing/playground/js/main.js
+++ src/imageProcessing/playground/js/main.js
@@ -23,6 +23,38 @@ let drawImage = function(cv, ctx, img) {
 }
 
 
+class RGBColor {
+    constructor(r, g, b) {
+      this.red = r;
+      this.green = g;
+      this.blue = b;
+    }
+}
+
+ 
+class MatrixImage {
+    constructor(imageData) {
+      this.imageData = imageData;
+      this.height = imageData.height;
+      this.width = imageData.width;
+    }
+    getPixel(x, y) {
+        let position = ((y * (this.width * 4)) + (x * 4));
+        return new RGBColor(
+             this.imageData.data[position],   //red
+             this.imageData.data[position+1], //green
+             this.imageData.data[position+2], //blue
+        );
+    }
+    setPixel(x, y, color) {
+        let position = ((y * (this.width * 4)) + (x * 4));
+        this.imageData.data[position] = color.red;
+        this.imageData.data[position+1] = color.green;
+        this.imageData.data[position+2] = color.blue;
+    }
+}
+
+
 let grayScale = function() {
     var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     var data = imageData.data;
@@ -80,9 +112,51 @@ let monoScale = function() {
     context.putImageData(imageData, 0, 0);
 }
 
+let redScale = function() {
+    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    let img = new MatrixImage(imageData);
+    for (var i = 0; i < img.width; i++) {
+        for (var j = 0; j < img.height; j++) {
+            var pixel = img.getPixel(i,j); 
+            img.setPixel(i, j, new RGBColor(pixel.red, 0, 0));
+        }
+    }
+    context.putImageData(img.imageData, 0, 0);
+}
+
+
+let greenScale = function() {
+    let imageData = contextCIE.getImageData(0, 0, canvasCIE.width, canvasCIE.height);
+    let img = new MatrixImage(imageData);
+    for (var i = 0; i < img.width; i++) {
+        for (var j = 0; j < img.height; j++) {
+            var pixel = img.getPixel(i,j);
+            img.setPixel(i, j, new RGBColor(0, pixel.green, 0));
+        }
+    }
+    contextCIE.putImageData(img.imageData, 0, 0);
+}
+
+
+let blueScale = function() {
+    let imageData = contextNTSC.getImageData(0, 0, canvasNTSC.width, canvasNTSC.height);
+    let img = new MatrixImage(imageData);
+    for (var i = 0; i < img.width; i++) {
+        for (var j = 0; j < img.height; j++) {
+            var pixel = img.getPixel(i,j);
+            img.setPixel(i, j, new RGBColor(0, 0, pixel.blue));
+        }
+    }
+    contextNTSC.putImageData(img.imageData, 0, 0);
+}
+
 
 document.getElementById('btn-load').addEventListener('click', load);
 document.getElementById('btn-grayscale').addEventListener('click', grayScale);
 document.getElementById('btn-grayscale-cie').addEventListener('click', grayScaleCIE);
 document.getElementById('btn-grayscale-ntsc').addEventListener('click', grayScaleNTSC);
 document.getElementById('btn-monochromatic').addEventListener('click', monoScale);
+// ---------------------------------------------------------------------------------------------
+document.getElementById('btn-red').addEventListener('click', redScale);
+document.getElementById('btn-green').addEventListener('click', greenScale);
+document.getElementById('btn-blue').addEventListener('click', blueScale);
