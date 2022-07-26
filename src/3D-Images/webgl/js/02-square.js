2d36ce63bae83a3bec2eb4d995ee8dff72b997cd
Luis Fernando Benatto <luisbenatto5@gmail.com>
2022-07-26 09:53:05 -0300

    WEBGL

---
+++ src/3D-Images/webgl/js/02-square.js
@@ -0,0 +1,62 @@
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+if (!gl) {
+    throw new Error('WebGL not supported');
+}
+
+// const vertexData = [
+//     -1,  1, 0,
+//      1,  1, 0,
+//     -1, -1, 0,
+//      1, -1, 0,
+// ];
+
+const vertexData = [
+    0,0.6,0,
+    -0.6,0.3,0,
+    0.6,0.3,0,
+    -0.6,-0.3,0,
+    0.6,-0.3,0,
+    0,-0.6,0,
+];
+
+// const vertexData = [
+//     1,  0, 0,
+//     0,  1, 0,
+//     0, -1, 0,
+//     -1,  0, 0,
+// ];
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, `
+attribute vec3 position;
+void main() {
+    gl_Position = vec4(position, 1);
+}
+`);
+gl.compileShader(vertexShader);
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, `
+void main() {
+    gl_FragColor = vec4(0.0744, 0.930, 0.274, 1);
+}
+`);
+gl.compileShader(fragmentShader);
+
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+const positionLocation = gl.getAttribLocation(program, `position`);
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+gl.useProgram(program);
+gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
